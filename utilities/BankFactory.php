15b9e33076e13ec66ac8e6bc00f496b1c3709ccc
Mohamed Elgebaly <36784871+M-Abd-ElBaset@users.noreply.github.com>
2026-01-29 16:41:29 +0200

    added bank factory

---
+++ utilities/BankFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace utilities;
+
+class BankFactory
+{
+    const BANKS = ['foodics', 'acme'];
+    public function create($name) : Bank
+    {
+
+        if(!in_array($name, self::BANKS)){
+            throw new \Exception("Invalid Bank");
+        }
+
+        return match($name) {
+            'foodics' => new FoodicsBank(),
+            'acme' => new AcmeBank(),
+        };
+    }
+}
