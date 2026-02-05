feba8d59860b5ec7d2183325666744b93cbc3166
Mohamed Elgebaly <36784871+M-Abd-ElBaset@users.noreply.github.com>
2026-02-05 16:26:11 +0200

    added webhook routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('webhook/foodics', [WebhookController::class, 'foodics']);
+Route::post('webhook/acme', [WebhookController::class, 'acme']);
